59bdcc2339d495850a098863dd41f8ff1fd60254
RaviKavida <kavida.ravikumar@gmail.com>
2020-06-25 16:25:19 +0530

    initail commit

---
+++ src/main/java/com/kubernates/docker/KuberNatesAndDocker/KuberNatesAndDockerApplication.java
@@ -0,0 +1,13 @@
+package com.kubernates.docker.KuberNatesAndDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberNatesAndDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuberNatesAndDockerApplication.class, args);
+	}
+
+}
